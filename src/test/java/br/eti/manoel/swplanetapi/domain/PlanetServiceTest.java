4c6028f7e7894f449b8bfa8f834598e4346be5ff
Manoel Lima <manoelsslima@yahoo.com.br>
2023-12-03 13:12:16 -0400

    First test passing.

--- src/test/java/br/eti/manoel/swplanetapi/domain/PlanetServiceTest.java
+++ src/test/java/br/eti/manoel/swplanetapi/domain/PlanetServiceTest.java
@@ -1,19 +1,27 @@
 package br.eti.manoel.swplanetapi.domain;
 
 import br.eti.manoel.swplanetapi.common.PlanetConstants;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 
 /**
- * @SpringBootTest - monta o contexto de aplicação do spring. Escaneia
+ * Anotação - SpringBootTest - monta o contexto de aplicação do spring. Escaneia
  * os beans e deixa disponível para os testes. É possível especificar a
  * classe que desejamos tornar disponível (reduzindo custos), com o atributo
  * "classes".
+ * <br>
+ * Anotar a classe com @SpringBootTest
+ * Criar os mocks das dependências (repositories) com @MockBean
+ * Informar os parãmetros do mock para
+ * <br>
+ * define o mock do sistema sob teste, define o stub dele e executa a lógica
+ * prinçipio AAA - arrange, act and assert
  */
 @SpringBootTest(classes = PlanetService.class)
 public class PlanetServiceTest {
@@ -21,12 +29,26 @@ public class PlanetServiceTest {
     @Autowired
     private PlanetService planetService;
 
+    /**
+     * Cria um dublê de teste
+     */
+    @MockBean
+    private PlanetRepository planetRepository;
+
     // operacao_estado_retorno
     @Test
     public void createPlanet_WithValidData_ReturnsPlanet() {
 
+        // AAA
+        // Arrange
+        // dizendo para "quando" o método save for chamado, passando PLANET, deve retornar PLANET
+        when(planetRepository.save(PlanetConstants.PLANET)).thenReturn(PlanetConstants.PLANET);
+
+        // Act
         // sut - system under test
         Planet sut = planetService.create(PlanetConstants.PLANET);
+
+        // Assert
         // implementar hahs and equals na classe Planet
         assertThat(sut).isEqualTo(PlanetConstants.PLANET);
 
