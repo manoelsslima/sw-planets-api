a47f39d1cfcac31a1242eec13954a01d107e4421
Manoel Lima <manoelsslima@yahoo.com.br>
2023-12-03 02:01:21 -0400

    Planet endpoint

---
+++ src/main/java/br/eti/manoel/swplanetapi/web/PlanetController.java
@@ -0,0 +1,27 @@
+package br.eti.manoel.swplanetapi.web;
+
+import br.eti.manoel.swplanetapi.domain.Planet;
+import br.eti.manoel.swplanetapi.domain.PlanetService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/planets")
+public class PlanetController {
+
+    private final PlanetService planetService;
+
+    public PlanetController(PlanetService planetService) {
+        this.planetService = planetService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Planet> create(@RequestBody Planet planet) {
+        Planet planetCreated = planetService.create(planet);
+        return ResponseEntity.status(HttpStatus.CREATED).body(planetCreated);
+    }
+}
