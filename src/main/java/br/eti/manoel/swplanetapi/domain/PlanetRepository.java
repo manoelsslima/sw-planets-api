c31ba786a6bc458dd930b200b200a6f304efa055
Manoel Lima <manoelsslima@yahoo.com.br>
2023-12-31 00:05:03 -0400

    get by name

--- src/main/java/br/eti/manoel/swplanetapi/domain/PlanetRepository.java
+++ src/main/java/br/eti/manoel/swplanetapi/domain/PlanetRepository.java
@@ -2,5 +2,8 @@ package br.eti.manoel.swplanetapi.domain;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface PlanetRepository extends CrudRepository<Planet, Long> {
+    Optional<Planet> findByName(String name);
 }
